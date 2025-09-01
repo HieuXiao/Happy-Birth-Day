376e0048eff737a08bf8cdc514794a059eef8a8f
MrHieu <hieunm.se@gmail.com>
2025-09-01 22:01:12 +0700

    added js

---
+++ js/flower.js
@@ -0,0 +1,21 @@
+onload = () => {
+  const c = setTimeout(() => {
+    document.body.classList.remove("not-loaded");
+
+    const titles = ('YEP!! HAPPY BIRTH DAY 🎂').split('')
+    const titleElement = document.getElementById('title');
+    let index = 0;
+
+    function appendTitle() {
+      if (index < titles.length) {
+        titleElement.innerHTML += titles[index];
+        index++;
+        setTimeout(appendTitle, 250); // 1000ms delay
+      }
+    }
+
+    appendTitle();
+
+    clearTimeout(c);
+  }, 1000);
+};
